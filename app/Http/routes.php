f8029e6052046c2a5fc02e27724ecee3998bd93e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-10-26 17:12:57 +0100

    Exemple amb Laravel Debugbar

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,15 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,11 +7,23 @@ Route::get('/', function () {
 
 
 Route::get('/resource', function () {
-
+    \Debugbar::startMeasure("resource");
     $authenticated = false;
+    Session::set('authenticated',true);
+    //dd(Session::all());
+    \Debugbar::info("Xivato1!!!!!");
+    \Debugbar::info(Session::all());
+    if (Session::has('authenticated')) {
+        if (Session::get('authenticated') == true ) {
+            $authenticated = true;
+        }
+    }
+
     if ($authenticated) {
+        \Debugbar::stopMeasure("resource");
         return view('resource');
     } else {
+        \Debugbar::stopMeasure("resource");
         return view('login');
     }
 
